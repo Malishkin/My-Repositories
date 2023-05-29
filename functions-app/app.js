a38ea17a6102a84e78467fd37b52dd4e7d8e1e5e
Sveta Lantsman <malishkin2002@gmail.com>
2023-05-29 19:48:14 +0300

    .

--- functions-app/app.js
+++ functions-app/app.js
@@ -7,7 +7,7 @@ const survey = {
       let answer = parseInt(prompt(this.question + '\n' + this.options.join('\n') + '\n(Enter option number)'));
       if (!isNaN(answer) && answer >= 0 && answer < this.options.length) {
         this.answers[answer]++;
-        this.printResults();
+        this.printResults('string');
       } else {
         alert('Invalid input. Please enter a valid option number.');
       }
@@ -20,5 +20,31 @@ const survey = {
         console.log(this.answers);
       }
     }
-  };
+};
   
+document.querySelector('#poll').addEventListener('click', survey.logNewAnswer.bind(survey));
+
+
+// survey.printResults.call({answers: [5, 2, 3]}, 'string');
+
+
+Замыкания (Closures)
+
+const safeBooking = function () {
+  let passengerCount = 0;
+
+  return function () {
+    passengerCount++;
+    console.log(`${passengerCount} passengers`);
+  };
+};
+
+const booker = safeBooking();
+
+booker();
+booker();
+booker();
+
+console.dir(booker);
+  
+
