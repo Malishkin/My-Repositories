4a8c34085ae84bd9ad89c80578e6dcb255dccb3b
Sveta Lantsman <malishkin2002@gmail.com>
2023-06-12 13:31:30 +0300

    .

--- async-await/script.js
+++ async-await/script.js
@@ -4,8 +4,33 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 //////////////////////////////////////////////////////
-
 const request = new XMLHttpRequest();
 request.open('GET', `https://restcountries.com/v3.1/name/ukraine`);
 
 request.send();
+
+request.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+
+  // map each currency object to its name
+  const currencies = Object.values(data.currencies)
+    .map(currency => currency.name)
+    .join(', ');
+  const languages = Object.values(data.languages).join(', ');
+
+  const html = `
+        <article class="country">
+        <img class="country__img" src="${data.flags.svg}" />
+        <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${data.population}</p>
+            <p class="country__row"><span>🗣️</span>${languages}</p>
+            <p class="country__row"><span>💰</span>${currencies}</p>
+        </div>
+        </article>
+    `;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
