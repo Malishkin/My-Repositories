468d6f5b63cf67f46bc9605787ab2aa2ae591c6e
Sveta Lantsman <malishkin2002@gmail.com>
2023-06-13 15:22:39 +0300

    .

--- async-await/script.js
+++ async-await/script.js
@@ -197,3 +197,36 @@ const lotteryPromise = new Promise(function (resolve, reject) {
   }, 3000);
 });
 lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+//Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+// wait(3).then(() => {
+//   console.log('I waited for 3 seconds');
+//   return wait(2).then(() => console.log('I waited for 2 seconds'));
+// });
+
+wait(1)
+  .then(() => {
+    console.log('Прошла 1 секунда');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('Прошла 2 секунда');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('Прошла 3 секунда');
+    return wait(1);
+  })
+  .then(() => {
+    console.log('Прошла 4 секунда');
+    return wait(1);
+  });
+
+Promise.resolve('Resolved!').then(res => console.log(res));
+Promise.reject(new Error('Rejected!')).catch(err => console.error(err));
