19bff10b5907c57e13e63b09833cfe67a2cc11e8
Sveta Lantsman <malishkin2002@gmail.com>
2023-06-13 09:21:03 +0300

    .

--- async-await/script.js
+++ async-await/script.js
@@ -5,7 +5,7 @@ const countriesContainer = document.querySelector('.countries');
 
 //////////////////////////////////////////////////////
 
-const getCountryData = function (country) {
+const getCountryAndBourderCountries = function (country) {
   const request = new XMLHttpRequest();
   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
 
@@ -14,35 +14,70 @@ const getCountryData = function (country) {
   request.addEventListener('load', function () {
     const [data] = JSON.parse(this.responseText);
     console.log(data);
+    //Отображение страны
+    displayCountry(data);
+    //Отображение первой граничащей страны
+    const [firstNeighbour] = data.borders;
+    if (!firstNeighbour) return;
+    // вызов AJAX для получения данных о первой соседней стране
+    const request2 = new XMLHttpRequest();
 
-    // map each currency object to its name
-    const currencies = Object.values(data.currencies)
-      .map(currency => currency.name)
-      .join(', ');
+    request2.open(
+      'GET',
+      `https://restcountries.com/v3.1/alpha/${firstNeighbour}`
+    );
+    request2.send();
+    request2.addEventListener('load', function () {
+      const [data2] = JSON.parse(this.responseText);
+      console.log(data2);
+      displayCountry(data2, 'neighbour');
+    });
+  });
+};
 
-    const currencies1 = Object.values(data.currencies)
-      .map(currency => currency.symbol)
-      .join(', ');
-    const languages = Object.values(data.languages).join(', ');
+const displayCountry = function (data, className = '') {
+  const currencies = Object.values(data.currencies)
+    .map(currency => `${currency.name} (${currency.symbol})`)
+    .join(', ');
 
-    const html = `
-        <article class="country">
+  const languages = Object.values(data.languages).join(', ');
+
+  const html = `
+      <article class="country ${className}">
         <img class="country__img" src="${data.flags.svg}" />
         <div class="country__data">
-            <h3 class="country__name">${data.name.common}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-              +data.population / 1000000
-            ).toFixed(1)} million</p>
-            <p class="country__row"><span>🗣️</span>${languages}</p>
-            <p class="country__row"><span>💰</span>${currencies1} ${currencies} 
-   </p>
+          <h3 class="country__name">${data.name.common}</h3>
+          <h4 class="country__region">${data.region}</h4>
+          <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+          ).toFixed(1)} million</p>
+          <p class="country__row"><span>🗣️</span>${languages}</p>
+          <p class="country__row"><span>💰</span>${currencies}</p>
         </div>
-        </article>
+      </article>
     `;
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      displayCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      if (!neighbour) return;
+
+      // Modify the fetch URL to correct API version
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then(response => {
+      if (response) return response.json();
+    })
+    .then(data => {
+      if (data) displayCountry(data[0], 'neighbour');
+    })
+    .catch(err => console.error(err));
 };
 
 getCountryData('ukraine');
