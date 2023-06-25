daefcd4a36771922f0d71c60be779f13cb9255cc
Sveta Lantsman <malishkin2002@gmail.com>
2023-06-25 11:25:58 +0300

    .

--- jquery-learn/app.js
+++ jquery-learn/app.js
@@ -9,22 +9,30 @@
 // // 3. Добавление элемента в DOM
 // document.body.appendChild(newDiv);
 
-// 1. Создание элемента
-var newDiv = $("<div></div>");
-
-// 2. Установка свойств для этого элемента
-newDiv.text("Привет, мир!");
-newDiv.attr("id", "myNewDiv");
-newDiv.addClass("someClass");
-
-// 3. Добавление элемента в DOM
-$("body").append(newDiv);
-
-var newButton = $("<button>Нажми меня</button>");
-
-newButton.css("background-color", "red");
-
-$("body").append(newButton);
-newButton.click(function () {
-  alert("Привет, мир!");
+// Создание элемента
+var container = $("<div></div>").css({
+  display: "flex",
+  "justify-content": "center",
+  "align-items": "center",
+  height: "100vh",
+  "flex-direction": "column",
 });
+
+var newDiv = $("<div>Привет, мир!</div>")
+  .attr("id", "myNewDiv")
+  .addClass("someClass");
+var newButton = $("<button>Нажми меня</button>")
+  .css({
+    "background-color": "red",
+    color: "white",
+    padding: "10px 20px",
+    border: "none",
+    "border-radius": "5px",
+    margin: "10px",
+  })
+  .click(function () {
+    alert("Привет, мир!");
+  });
+
+container.append(newDiv, newButton);
+$("body").append(container);
